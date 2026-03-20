f47fa40602e7270665b30e4b436763492e7c1291
LEE LUNA <seojilee@student.42seoul.kr>
2026-03-21 00:14:29 +0900

    env installation files

--- attachment/create.sh
+++ attachment/create.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+read -p 'Enter your image file name (.iso): ' IMG
+
 qemu-img create -f qcow2 ubuntu_disk.qcow2 20G
-qemu-system-x86_64 -m 4G -drive file=ubuntu_disk.qcow2 -cdrom ubuntu-24.04.4-live-server-amd64.iso -boot d
+qemu-system-x86_64 -m 4G -drive file=ubuntu_disk.qcow2 -cdrom $IMG -boot d
