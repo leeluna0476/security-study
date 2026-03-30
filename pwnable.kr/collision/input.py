0b2e0aa64b5e976d145eae6d09a23229b1346696
LEE LUNA <seojilee@student.42seoul.kr>
2026-03-30 19:04:57 +0900

    toddler's bottle: fd, collision

---
+++ pwnable.kr/collision/input.py
@@ -0,0 +1,13 @@
+import sys
+
+def print_in_char(a):
+    for i in range(0, 4):
+        sys.stdout.buffer.write(
+                bytes([(a >> (i * 8)) & 0xff])
+                )
+
+h = int("0x21DD09EC", 16)
+a = h // 5 + 1
+for _ in range(4):
+    print_in_char(a)
+print_in_char(h // 5)
